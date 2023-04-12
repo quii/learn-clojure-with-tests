c1855a82ff10396b8aee0852fc4614fac60cc302
Chris James <chris.james@springernature.com>
2023-04-12 08:28:15 +0100

    more sequence studies

--- test/learn_clojure_with_tests/sequences.clj
+++ test/learn_clojure_with_tests/sequences.clj
@@ -3,36 +3,56 @@
 
 ;; sequences are the mega-abstraction around lots of data types. It has a rich library of functions that work with them.
 
-(defn can-create-vector [data] (is (= data [0 1 2])))
-(defn can-create-vector-of-evens [data] (is (= data [0 2 4 6 8])))
+(defn can-create-sequence [data] (is (= data [0 1 2])))
+(defn can-create-sequence-of-evens [data] (is (= data [0 2 4 6 8])))
 
 (deftest generating-vectors "we can generate vectors in different ways"
-                         (can-create-vector (range 3))
-                         (can-create-vector (vec (range 3)))
-                         (can-create-vector (into [0 1] [2]))
-                         (can-create-vector (into [] (range 3)))
-                         (can-create-vector (rest [10000000000 0 1 2]))
-                         (can-create-vector-of-evens (range 0 10 2))
-                         )
+  (can-create-sequence (range 3))
+  (can-create-sequence (vec (range 3)))
+  (can-create-sequence (vector 0 1 2))
+  (can-create-sequence (into [0 1] [2]))
+  (can-create-sequence (into [] (range 3)))
+  (can-create-sequence (rest [10000000000 0 1 2]))
+  (can-create-sequence-of-evens (range 0 10 2))
+  (testing "note conj adds to the end"
+    (can-create-sequence (conj [0 1] 2))
+    (can-create-sequence (conj [0] 1 2)))
+  )
+
+(deftest can-create-lists "we can generate lists in different ways"
+  (can-create-sequence (list 0 1 2))
+  (can-create-sequence `(0 1 2))
+  (testing "conj adds to the start with lists"
+    (can-create-sequence (conj (list 1 2) 0))
+    (can-create-sequence (conj (list 2) 1 0))))
 
 (defn can-sum [sum] (is (= 10 (sum (range 5)))))
 (deftest reducing-sequences "we can reduce sequences in different ways"
-                              (can-sum (fn [x] (reduce + x)))
-                              (can-sum (fn [x] (apply + x))))
+  (can-sum (fn [x] (reduce + x)))
+  (can-sum (fn [x] (apply + x))))
 
 ;; see here https://clojure.org/api/cheatsheet
 (def some-vec [1 2 3])
 (def some-list `(1 2 3))
 (def some-set #{1 2 3})
-(deftest count-test "we can count all sequences"
-                    (is (= 3 (count some-vec)))
-                    (is (= 3 (count some-list)))
-                    (is (= 3 (count some-set))))
 
-(defn can-check-is-not-empty [col]
-  (is (not-empty col)))
-
-(deftest not-empty-test "we can check if any sequence is not empty"
-                        (can-check-is-not-empty some-vec)
-                        (can-check-is-not-empty some-list)
-                        (can-check-is-not-empty some-set))
+(deftest useful-seq-functions-test "there are many operations common to seq, here are a few"
+  (testing "count"
+    (are [data] (= 3 (count data))
+                some-vec
+                some-list
+                some-set))
+  (testing "is-empty"
+    (is (empty? []))
+    (not (empty? some-vec))
+    (is empty? #{})
+    (not (empty? some-set)))
+  (testing "first"
+    (are [data] (= 1 (first data))
+                some-vec
+                some-list
+                some-set))
+  (testing "rest"
+    (are [data] (= [2 3] (rest data))
+                some-vec
+                some-list)))
