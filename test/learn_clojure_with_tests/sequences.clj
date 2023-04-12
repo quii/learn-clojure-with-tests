01d24cfee73100a0afa565017c1b9ac587a38ffa
Chris James <chris.james@springernature.com>
2023-04-12 08:05:31 +0100

    sequences is the proper name, not collections

--- test/learn_clojure_with_tests/sequences.clj
+++ test/learn_clojure_with_tests/sequences.clj
@@ -1,10 +1,12 @@
-(ns learn_clojure_with_tests.generic_collections
+(ns learn_clojure_with_tests.sequences
   (:require [clojure.test :refer :all]))
+
+;; sequences are the mega-abstraction around lots of data types. It has a rich library of functions that work with them.
 
 (defn can-create-vector [data] (is (= data [0 1 2])))
 (defn can-create-vector-of-evens [data] (is (= data [0 2 4 6 8])))
 
-(deftest generating-data "we can generate data in different ways"
+(deftest generating-vectors "we can generate vectors in different ways"
                          (can-create-vector (range 3))
                          (can-create-vector (vec (range 3)))
                          (can-create-vector (into [0 1] [2]))
@@ -14,7 +16,7 @@
                          )
 
 (defn can-sum [sum] (is (= 10 (sum (range 5)))))
-(deftest reducing-collections "we can reduce collections in different ways"
+(deftest reducing-sequences "we can reduce sequences in different ways"
                               (can-sum (fn [x] (reduce + x)))
                               (can-sum (fn [x] (apply + x))))
 
@@ -22,7 +24,7 @@
 (def some-vec [1 2 3])
 (def some-list `(1 2 3))
 (def some-set #{1 2 3})
-(deftest count-test "we can count all collections"
+(deftest count-test "we can count all sequences"
                     (is (= 3 (count some-vec)))
                     (is (= 3 (count some-list)))
                     (is (= 3 (count some-set))))
@@ -30,7 +32,7 @@
 (defn can-check-is-not-empty [col]
   (is (not-empty col)))
 
-(deftest not-empty-test "we can check if any collection is not empty"
+(deftest not-empty-test "we can check if any sequence is not empty"
                         (can-check-is-not-empty some-vec)
                         (can-check-is-not-empty some-list)
                         (can-check-is-not-empty some-set))
