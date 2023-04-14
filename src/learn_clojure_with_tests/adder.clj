c0c9904bdadc24a49daeee301a7c322c8f12ff6e
Chris James <chris.james@springernature.com>
2023-04-14 09:37:18 +0100

    notes on repl

--- src/learn_clojure_with_tests/adder.clj
+++ src/learn_clojure_with_tests/adder.clj
@@ -6,3 +6,21 @@
 
 (defn add-5-with-threading [x]
   (-> x inc inc inc inc inc))
+
+(defn add-1
+  "adds 1"
+  [x]
+  (+ x 1))
+
+(defn multiply-by-2
+  [x] (* x 2))
+
+(defn print-number
+  [num]
+  (format "This numberr is %d" num))
+
+;repl notes
+;all are roughly around shift+option
+;L will load file
+;M will sync file
+;P will send current form and run it
