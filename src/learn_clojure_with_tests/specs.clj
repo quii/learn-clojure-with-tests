a7514912a950ef6a808bbb1139844ed7b1916aa4
Chris James <chris.james@springernature.com>
2023-04-14 11:14:40 +0100

    more specs

--- src/learn_clojure_with_tests/specs.clj
+++ src/learn_clojure_with_tests/specs.clj
@@ -13,3 +13,30 @@
 ; remember to send to repl for fun
 (s/valid? ::area "bob")
 (s/valid? ::area 1)
+
+; can define valid enumerated values with sets, which is nice
+(s/def ::submission #{:awaiting-qc :awaiting-handling-editor :etc})
+(s/valid? ::submission :awaiting-handling-editor)
+(s/valid? ::submission :pie)
+
+; logical specs
+(s/def ::odd-int (s/and int? odd?))
+(s/valid? ::odd-int 5)
+(s/valid? ::odd-int 10)
+(s/valid? ::odd-int 5.2)
+
+; feels like the combination of specs can be pretty rad
+(s/def ::odd-or-42 (s/or ::odd-int #{42}))
+(s/valid? ::odd-or-42 42)
+
+; you can label the things to get useful output from conform
+(s/def ::chris-or-james (s/or ::chris-matched #{"chris"} ::james-matched #{"james"}))
+(s/valid? ::chris-or-james "chris")
+(s/conform ::chris-or-james "james")
+; => [:learn_clojure_with_tests.specs/james-matched "james"]
+
+; range specs
+(s/def ::bowlingrange (s/int-in 0 11))
+
+; collection specs
+(s/def ::colofcjs (s/coll-of ::chris-or-james))
--- test/learn_clojure_with_tests/specs_test.clj
+++ test/learn_clojure_with_tests/specs_test.clj
@@ -4,6 +4,21 @@
             [clojure.test :refer :all]))
 
 (deftest validating-specs
-         (testing "we can create specs and then validate data"
-                  (is (s/valid? :learn_clojure_with_tests.specs/area 2))
-                  (not (s/valid? :learn_clojure_with_tests.specs/area "two"))))
+  (testing "we can create specs and then validate data"
+    (is (s/valid? :learn_clojure_with_tests.specs/area 2))
+    (is (not (s/valid? :learn_clojure_with_tests.specs/area "two"))))
+  (testing "enumerated values"
+    (is (s/valid? :learn_clojure_with_tests.specs/submission :awaiting-handling-editor))
+    (is (not (s/valid? :learn_clojure_with_tests.specs/submission :poo))))
+  (testing "logical specs"
+    (is (s/valid? :learn_clojure_with_tests.specs/odd-int 9))
+    (is (not (s/valid? :learn_clojure_with_tests.specs/odd-int 2))))
+  (testing "labelling"
+    (is (= [:learn_clojure_with_tests.specs/james-matched "james"]
+           (s/conform :learn_clojure_with_tests.specs/chris-or-james "james"))))
+  (testing "ranges"
+    (is (s/valid? :learn_clojure_with_tests.specs/bowlingrange 10))
+    (is (not (s/valid? :learn_clojure_with_tests.specs/bowlingrange 20))))
+  (testing "collections"
+    (is (s/valid? :learn_clojure_with_tests.specs/colofcjs ["chris" "chris" "james"]))
+    (is (not (s/valid? :learn_clojure_with_tests.specs/colofcjs ["chris" "james" "nicky"])))))
